36a4c38cc7b23acfa8d36eb08b9f5af15ebe1944
Pavithrabalakumaran <pavithrachandrasekar9360@gmail.com>
2023-09-13 14:37:39 +0000

    feedback

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,77 @@
+// Write your React code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class Feedback extends Component {
+  state = {
+    isFeedbackSelected: false,
+  }
+
+  onClickEmoji = () => this.setState({isFeedbackSelected: true})
+
+  renderFeedbackQuestion = () => {
+    const {resources} = this.props
+    const {emojis} = resources
+
+    return (
+      <div className="first-container">
+        <div className="inner-container">
+          <h1 className="question">
+            How satisfied are you with our customer support performance?
+          </h1>
+          <ul className="emoji-container">
+            {emojis.map(emoji => (
+              <li key={emoji.id}>
+                <button
+                  type="button"
+                  className="emoji-btn"
+                  onClick={this.onClickEmojiButton}
+                >
+                  <img
+                    src={emoji.imageUrl}
+                    alt={emoji.name}
+                    className="emoji"
+                  />
+                  <br />
+                  <span className="emoji-name">{emoji.name}</span>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+  renderThankYouScreen = () => {
+    const {resources} = this.props
+    const {loveEmojiUrl} = resources
+
+    return (
+      <div className="inner-container">
+        <img src={loveEmojiUrl} alt="love emoji" className="love-emoji" />
+        <h1 className="thank-you-text">Thank You!</h1>
+        <p className="description">
+          We will use your feedback to improve our customer support performance.
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isFeedbackSelected} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="feedback-card">
+          {isFeedbackSelected
+            ? this.renderThankYouScreen()
+            : this.renderFeedbackQuestion()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Feedback
